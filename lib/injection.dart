1c59a33f92b2459d1e4307bb27b9a4699b9eab40
sherifhasan <sherif.hasan1994@gmail.com>
2024-09-12 19:03:43 +0200

    initialize AbsenceCubit in main file

--- lib/injection.dart
+++ lib/injection.dart
@@ -6,14 +6,15 @@ import 'infrastructure/app_repository_imp.dart';
 import 'infrastructure/datasource/data_source.dart';
 import 'infrastructure/datasource/local_data_source.dart';
 
-final GetIt getIt = GetIt.instance;
+final GetIt injection = GetIt.instance;
 
 Future<void> setUp() async {
   await initLocators();
 }
 
 Future<void> initLocators() async {
-  getIt.registerSingleton<DataSource>(LocalDataSource());
-  getIt.registerSingleton<AppRepository>(AppRepositoryImp(getIt.get()));
-  getIt.registerSingleton<AbsenceCubit>(AbsenceCubit(repository: getIt.get()));
+  injection.registerSingleton<DataSource>(LocalDataSource());
+  injection.registerSingleton<AppRepository>(AppRepositoryImp(injection.get()));
+  injection.registerSingleton<AbsenceCubit>(
+      AbsenceCubit(repository: injection.get()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:absence_manager/application/absence_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'injection.dart';
 
 void main() {
+  setUp(); // Initialize the service locator
   runApp(const MyApp());
 }
 
@@ -9,11 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (_) => injection<AbsenceCubit>()..loadInitialData(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
       ),
     );
   }
