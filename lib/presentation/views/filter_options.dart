ef3e48e3fa34877e0bb02844b948d6f880fc544b
sherifhasan <sherif.hasan1994@gmail.com>
2024-09-13 19:09:08 +0200

    optimized filtering in filter options view

--- lib/presentation/views/filter_options.dart
+++ lib/presentation/views/filter_options.dart
@@ -2,42 +2,49 @@ import 'package:flutter/material.dart';
 
 class FilterOptions extends StatelessWidget {
   final VoidCallback filterByDate;
-  final void Function(String) filterByAbsencesByType;
+  final ValueChanged<String> filterByAbsencesByType;
   final VoidCallback loadAllAbsences;
+  final String selectedFilter;
 
-  const FilterOptions(
-      {required this.filterByDate,
-      required this.filterByAbsencesByType,
-      required this.loadAllAbsences,
-      super.key});
+  const FilterOptions({
+    super.key,
+    required this.filterByDate,
+    required this.filterByAbsencesByType,
+    required this.loadAllAbsences,
+    required this.selectedFilter,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final filters = [
+      {'label': 'All', 'value': 'all', 'onPressed': loadAllAbsences},
+      {
+        'label': 'Vacation',
+        'value': 'vacation',
+        'onPressed': () => filterByAbsencesByType('vacation')
+      },
+      {
+        'label': 'Sickness',
+        'value': 'sickness',
+        'onPressed': () => filterByAbsencesByType('sickness')
+      },
+      {'label': 'Date', 'value': 'date', 'onPressed': filterByDate},
+    ];
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ElevatedButton(
-              onPressed: loadAllAbsences,
-              child: const Text('All'),
+      child: Wrap(
+        spacing: 8.0,
+        children: filters.map((filter) {
+          final isSelected = selectedFilter == filter['value'];
+          return ElevatedButton(
+            onPressed: filter['onPressed'] as VoidCallback,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: isSelected ? Colors.blueAccent : null,
             ),
-            ElevatedButton(
-              onPressed: () => filterByAbsencesByType('vacation'),
-              child: const Text('Filter by Vacation'),
-            ),
-            ElevatedButton(
-              onPressed: () => filterByAbsencesByType('sickness'),
-              child: const Text('Filter by Sickness'),
-            ),
-            ElevatedButton(
-              onPressed: filterByDate,
-              child: const Text('Filter by Date'),
-            ),
-          ],
-        ),
+            child: Text(filter['label'] as String),
+          );
+        }).toList(),
       ),
     );
   }
